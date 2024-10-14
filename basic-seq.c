2007a574f7744087d79d12eea870e36f9602f59d
Thomas Parsons <thomascparsons@gmail.com>
2024-10-14 21:00:17 +0100

    ran on linux + removed null char from write to file

--- basic-seq.c
+++ basic-seq.c
@@ -167,7 +167,7 @@ void *writeResultsToFile(double *output, int numOfPoints, int numOfFeatures, cha
 			if(j < numOfFeatures - 1) fprintf(file, "%lf,", output[i * numOfFeatures + j]);
 			else fprintf(file, "%lf", output[i * numOfFeatures + j]);
 		}
-		fprintf(file, "\n\0");
+		fprintf(file, "\n");
         
     }
 
